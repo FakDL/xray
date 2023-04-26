bebf2aed0a6f1f7920aa3cb3be582c134196ddbb
Danil Asulgaraev <danil.asulgaraev@waveaccess.ru>
2023-04-26 16:12:05 +0300

    xray app initial commit

---
+++ app/src/main/java/com/example/xray/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.xray
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.xray.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        toFragment(DiseaseRecognitionFragment.getInstance())
+    }
+
+    private fun toFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.am_container, fragment)
+            .commit()
+    }
+
+}
